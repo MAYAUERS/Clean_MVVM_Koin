fc29072c5dfb4699faccd85a0339a2dba0f88ae0
MAYAUERS <meerapotdar999@gmail.com>
2022-02-14 06:56:24 +0530

    Changes done for Application class

--- app/src/main/java/com/example/clean_arch_with_koin/BaseApplication.kt
+++ app/src/main/java/com/example/clean_arch_with_koin/BaseApplication.kt
@@ -3,7 +3,9 @@ package com.example.clean_arch_with_koin
 import android.app.Application
 import com.example.clean_arch_with_koin.di.modules.*
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class BaseApplication :Application() {
 
@@ -11,6 +13,7 @@ class BaseApplication :Application() {
         super.onCreate()
 
         startKoin {
+            androidLogger(Level.DEBUG)
             androidContext(this@BaseApplication)
             modules(listOf(retrofitModule, presentationModule, picassoModule))
         }
